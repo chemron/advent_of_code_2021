23300165437abda81662164da3940b9e2035e205
chemron <camerontasmith@gmail.com>
2021-12-02 16:16:19 +1100

    day 2

--- day_02/day_02_pt_2.rb
+++ day_02/day_02_pt_2.rb
@@ -5,20 +5,22 @@ input = File.open("input.txt")
 
 instructions = input.readlines.map {|x| x.chomp.split(" ")}
 
-vertical_distance = 0
+depth = 0
 horizontal_distance = 0
+aim = 0
 
 instructions.each do |direction, value|
     value = value.to_i
     if direction=="forward"
         horizontal_distance += value
+        depth += aim * value
     elsif direction == "up"
-        vertical_distance -= value
+        aim -= value
     elsif direction == "down"
-        vertical_distance += value
+        aim += value
     else
         puts "error, direction is #{direction}"
     end
 end
 
-puts vertical_distance * horizontal_distance
+puts depth * horizontal_distance
