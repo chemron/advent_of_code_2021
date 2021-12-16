04628680366a9f3594b1040e4c6fc5f589fa0568
chemron <camerontasmith@gmail.com>
2021-12-16 16:54:54 +1100

    day 6 done

--- day_06/day_06.rb
+++ day_06/day_06.rb
@@ -1,22 +1,36 @@
 # https://adventofcode.com/2021/day/6
 
-input = File.read("test_input.txt")
-initial_state = input.split(",").map(&:to_i)
+input = File.read("input.txt").split(",").map(&:to_i)
+
+# initial state
+# state[time] = # of fish with timer = time
+current_state = Array.new(9, 0)
+input.each {|time| current_state[time] += 1 }
+
 
 def next_state(current_state)
-    new_fish = 0
-    next_state = current_state.map do |timer|
-        if timer == 0
-            new_fish += 1
-            6
+    next_state = Array.new(9, 0)
+    current_state.each_with_index do |n_fish, time|
+        if time == 0
+            # fish been born
+            next_state[8] += n_fish
+            # fish reseting timmer
+            next_state[6] += n_fish
         else
-            timer - 1
+            next_state[time - 1] += n_fish
         end
     end
-    next_state += Array.new(new_fish, 8)
     return next_state
 end
 
-inificient
 
-puts next_state(next_state(initial_state))
+update = Proc.new {current_state = next_state(current_state)}
+# run 80 times
+80.times &update
+n_fish = current_state.sum
+puts "After 80 days, there are #{n_fish} fish"
+
+# run 256 times
+(256 - 80).times &update
+n_fish = current_state.sum
+puts "After 256 days, there are #{n_fish} fish"
