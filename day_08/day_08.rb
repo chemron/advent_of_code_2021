f3e1abfe826fbb289d4f6b321d4a0ed91dc245f3
chemron <camerontasmith@gmail.com>
2021-12-18 14:07:20 +1100

    done! (finally)

--- day_08/day_08.rb
+++ day_08/day_08.rb
@@ -39,7 +39,7 @@ def get_segments(pattern)
             # if we have found the segment for the given wire, this segment cannot correspond
             # to any other wires
             if @segment_hash[wire].length == 1
-                update_segements(wire)
+                update_segments(wire)
             end
         end
     end
@@ -48,45 +48,100 @@ end
 
 # if we have found the segment corresponding to wire, this segment cannot correspond
 # to any other wires
-def update_segements(wire)
+def update_segments(wire)
     (:a..:g).each do |key|
         if (key != wire) & (@segment_hash[key].length != 1)
             @segment_hash[key] -= @segment_hash[wire]
 
-            (@segment_hash[key].length == 1)? update_segements(key) : nil
+            (@segment_hash[key].length == 1)? update_segments(key) : nil
 
         end
     end
 end
 
 
+# decode pattern with the segment hash
+def decode_pattern(pattern)
+    segments = pattern.chars.map {|wire| @segment_hash_inverse[wire.to_sym]}
+    segments = segments.to_set
 
-input = File.open("test_input2.txt")
+    if segments == Set[:c, :f]
+        @ones += 1
+        return 1
+    elsif segments == Set[:a, :c, :d, :e, :g]
+        return 2
+    elsif segments ==  Set[:a, :c, :d, :f, :g]
+        return 3
+    elsif segments ==  Set[:b, :c, :d, :f]
+        @fours += 1
+        return 4
+    elsif segments ==  Set[:a, :b, :d, :f, :g]
+        return 5
+    elsif segments ==  Set[:a, :b, :d, :e, :f, :g]
+        return 6
+    elsif segments ==  Set[:a, :c, :f]
+        @sevens += 1
+        return 7
+    elsif segments ==  Set[:a, :b, :c, :d, :e, :f, :g]
+        @eights += 1
+        return 8
+    elsif segments ==  Set[:a, :b, :c, :d, :f, :g]
+        return 9
+    elsif segments ==  Set[:a, :b, :c, :e, :f, :g]
+        return 0
+    else
+        puts @segment_hash_inverse
+        puts segments
+        puts "Error!"
+        return "nah man"
+    end
+end
+
+
+input = File.open("input.txt")
 lines = input.readlines
 
 lines.map! {|line| line.split(" | ")}
 
 
-ones = 0
-fours = 0
-sevens = 0
-eights = 0
+@ones = 0
+@fours = 0
+@sevens = 0
+@eights = 0
+@output_total = 0
+
 lines.each do |signal, output|
 
-    # hash table of what segements could correspond to what wires
+    # hash table of what segments could correspond to what wires
     # initially any segment could correspond to a given wire
     @segment_hash = Hash.new(Set[:a, :b, :c, :d, :e, :f, :g])
 
-    patterns = signal.split(" ") + output.split(" ")
-    patterns.each do |pattern|
-        get_segments(pattern)
+    output_patterns = output.split(" ")
+    signal_patterns = signal.split(" ")
+
+    # find corresponding segments for each wire
+    patterns = signal_patterns + output_patterns
+    patterns.each {|pattern| get_segments(pattern)}
+
+    # replace sets with symbols
+    @segment_hash.each do |wire, segment|
+        if segment.length != 1
+            puts "Error, with wire: #{wire}, multiple segments: #{segment}"
+        end
+        @segment_hash[wire] = segment.to_a[0]
     end
 
-    
-    puts @segment_hash
+    # inverse of the segment hash
+    @segment_hash_inverse = @segment_hash.invert
+    output_values = output_patterns.map {|pattern| decode_pattern(pattern)}
+    output_value = output_values.join("").to_i
+    @output_total += output_value
 end
 
 
 # part one
-easy_ones = ones + fours + sevens + eights
+easy_ones = @ones + @fours + @sevens + @eights
 puts "1, 4, 7, or 8 appear #{easy_ones} times"
+
+# part two
+puts "Total output sum is: #{@output_total}"
